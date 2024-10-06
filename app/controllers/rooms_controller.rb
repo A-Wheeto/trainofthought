da5f01a5d7f10b8f62951ea10a7ee925c0bf743a
Adam Wheatley <adamwheatley7@gmail.com>
2024-10-06 18:56:34 +0000

    Basic room controller actions Added some bootstrap for basic layout

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -7,4 +7,18 @@ class RoomsController < ApplicationController
         @rooms = Room.all
         @users = User.all_except(@current_user)
     end
+
+    def show
+        @current_user = current_user
+        @single_room = Room.find(params[:id])
+        @rooms = Room.all
+        @users = User.all_except(@current_user)
+        @room = Room.new
+      
+        render "index"
+    end
+
+    def create
+        @room = Room.create(name: params["name"])
+    end
 end
